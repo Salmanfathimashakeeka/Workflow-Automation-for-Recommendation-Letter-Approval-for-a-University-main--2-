5780e452584b9d26db369fa6873b8b7e64f8a799
sunshine <fathimashakeeka345@gmail.com>
2024-11-22 22:52:45 +0530

    updated the page with for for staff login

--- Workflow-Automation-for-Recommendation-Letter-Approval-for-a-University-main/frontend/src/pages/Stafflogin.js
+++ Workflow-Automation-for-Recommendation-Letter-Approval-for-a-University-main/frontend/src/pages/Stafflogin.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState} from "react";
 
 const Stafflogin = () => {
 
@@ -14,10 +14,22 @@ const Stafflogin = () => {
 
 
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div >
+      <h2>Staff Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div >
+          <label>Staff ID:</label>
+          <input type="text" value={staffId} onChange={(e) => setStaffId(e.target.value)} placeholder="Enter your staff ID" required/>
+        </div>
+        <div >
+          <label>Password:</label>
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}  placeholder="Enter your password" required/>
 
-export default Stafflogin
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Stafflogin;
