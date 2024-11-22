3abdc72bab415a11ca78cbf2afc56f4340112812
sunshine <fathimashakeeka345@gmail.com>
2024-11-22 22:43:20 +0530

    starting with stafflogin

--- Workflow-Automation-for-Recommendation-Letter-Approval-for-a-University-main/frontend/src/pages/Studentlogin.js
+++ Workflow-Automation-for-Recommendation-Letter-Approval-for-a-University-main/frontend/src/pages/Studentlogin.js
@@ -1,1 +1,23 @@
+import React, { useState} from "react";
+ 
+ const Studentlogin = () => {
+
+    const [staffId, setStaffId] = useState(""); 
+    const [password, setPassword] = useState("");
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      console.log("Staff Login Details:", { staffId, password });
+      
+    };
+  
+
+   return (
+     <div>
+       
+     </div>
+   )
+ }
+ 
+ export default StaffLogin;
  
